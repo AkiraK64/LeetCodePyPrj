402e335c73dddcc895ce1468ebd333f6027cb8ed
DuongTT <trinhtungduong5b1@gmail.com>
2025-12-08 08:28:29 +0700

    Do CD: 4A

---
+++ CFSolution.py
@@ -0,0 +1,7 @@
+class CFSolution:
+    def __init__(self):
+        pass
+    # 4A - Watermelon
+    def solution_4A():
+        w = int(input())
+        print("YES" if w % 2 == 0 and w > 3 else "NO")
