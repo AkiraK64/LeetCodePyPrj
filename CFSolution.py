6fcf58043a5e5dcff3f282c7c11088c01622fb36
DuongTT <trinhtungduong5b1@gmail.com>
2025-12-08 08:48:12 +0700

    Do CF: 71A

--- CFSolution.py
+++ CFSolution.py
@@ -5,3 +5,12 @@ class CFSolution:
     def solution_4A():
         w = int(input())
         print("YES" if w % 2 == 0 and w > 3 else "NO")
+    # 71A - Way Too Long Words
+    def solution_71A():
+        n = int(input())
+        for _ in range(n):
+            st = input()
+            if len(st) <= 10:
+                print(st)
+            else:
+                print(st[0] + str(len(st)-2) + st[-1])
